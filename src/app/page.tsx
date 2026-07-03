0236e4832918789d98057c307a0dd1b6f8b6f0d4
E-MUSK-OS <2383elonmusk@gmail.com>
2026-07-03 18:07:40 +0530

    Fix Type Error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-red-100">
+  const router = useRouter();
 
-      <h1 className="text-4xl font-bold text-red-600">
-        Tailwind is working!
-      </h1>
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    const refreshToken = localStorage.getItem("refreshToken");
+
+    if (accessToken && refreshToken) {
+      router.replace("/dashboard");
+    } else {
+      router.replace("/login");
+    }
+  }, [router]);
+
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <p className="text-lg font-medium">Loading...</p>
     </main>
-  )
+  );
 }
