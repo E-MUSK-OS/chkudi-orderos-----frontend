83ec415bb61ae774bb4347d1b1249e506131da87
E-MUSK-OS <2383elonmusk@gmail.com>
2026-07-06 15:20:40 +0530

    Fix error vms page

--- src/app/dashboard/vms/page.tsx
+++ src/app/dashboard/vms/page.tsx
@@ -1,5 +1,4 @@
 import Dashboard from "@/components/Dashboard/vms/Dashboard/Dashboard";
-import Vms from "@/components/Dashboard/vms/Vms";
 
 export default function VmsPage() {
   return <Dashboard />;
