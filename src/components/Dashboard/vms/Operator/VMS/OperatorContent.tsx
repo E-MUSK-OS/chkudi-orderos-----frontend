d88c276e073e618654941724fdb171a447490e17
E-MUSK-OS <2383elonmusk@gmail.com>
2026-07-15 13:58:16 +0530

    add scan tracking id

--- src/components/Dashboard/vms/Operator/VMS/OperatorContent.tsx
+++ src/components/Dashboard/vms/Operator/VMS/OperatorContent.tsx
@@ -28,28 +28,61 @@ const OperatorContent = ({ onLogout }: OperatorContentProps) => {
   const recorder = useRecorder();
   const scanner = useScanner();
 
+  const [operator, setOperator] = React.useState<{
+    operatorName: string;
+  } | null>(null);
+
+  const [selectedAccount, setSelectedAccount] = React.useState<{
+    accountName: string;
+  } | null>(null);
+
+  React.useEffect(() => {
+    const operatorData = sessionStorage.getItem("operator");
+    const accountData = sessionStorage.getItem("selectedAccount");
+
+    if (operatorData) {
+      setOperator(JSON.parse(operatorData));
+    }
+
+    if (accountData) {
+      setSelectedAccount(JSON.parse(accountData));
+    }
+  }, []);
+
   return (
     <>
-      <div className="mb-4 flex justify-between">
+      {/* <div className="mb-4 flex justify-between">
         <div className="w-full">
           <ScannerInput camera={camera} recorder={recorder} scanner={scanner} />
         </div>
-        {/* <div>
-          <button
-            type="button"
-            onClick={onLogout}
-            className="bg-[#0A0E1A] px-5 py-3 text-white text-lg"
-          >
-            Operato Logout
-          </button>
-        </div> */}
+      </div> */}
+      <div className="mb-4 flex gap-4">
+        {/* Scanner */}
+        <div className="flex-1">
+          <ScannerInput camera={camera} recorder={recorder} scanner={scanner} />
+        </div>
+
+        <div className="w-80">
+
+          <div className="flex gap-3">
+            <div className="border px-5 py-3 w-40">
+              <p className="text-sm text-gray-500">Operator</p>
+              <p className=" text-gray-900 text-lg">
+                {operator?.operatorName ?? "-"}
+              </p>
+            </div>
+
+            <div className="border px-5 py-3 w-40">
+              <p className="text-sm text-gray-500">Account</p>
+              <p className="text-gray-900 text-lg">
+                {selectedAccount?.accountName ?? "-"}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
 
-      {/* <SystemStatus /> */}
-
       <CameraPreview camera={camera} />
-
-      {/* <ScannerInput camera={camera} recorder={recorder} scanner={scanner} /> */}
 
       <UploadQueue />
 
