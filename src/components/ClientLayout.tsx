d7ce66e976f23a00325770e4c0962dac7c05f906
E-MUSK-OS <2383elonmusk@gmail.com>
2026-08-05 09:48:40 +0530

    add marketplace

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -1,9 +1,77 @@
+// "use client";
+
+// import { Toaster } from "sonner";
+// import ReactQueryProvider from "@/providers/ReactQueryProvider";
+// import { useEffect } from "react";
+// import { useRouter, usePathname, useSearchParams } from "next/navigation";
+// import AccountLockChecker from "./AccountLockChecker";
+
+// export default function ClientLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   const router = useRouter();
+
+//   const pathname = usePathname();
+//   const searchParams = useSearchParams();
+
+//   useEffect(() => {
+//     const currentUrl =
+//       pathname + (searchParams.toString() ? `?${searchParams.toString()}` : "");
+//     if (pathname !== "/offline") {
+//       sessionStorage.setItem("lastVisitedPage", currentUrl);
+//     }
+//   }, [pathname, searchParams]);
+
+//   useEffect(() => {
+//     // 🔥 Check initial status
+//     if (!navigator.onLine) {
+//       router.push("/offline");
+//     }
+
+//     const handleOffline = () => {
+//       router.push("/offline");
+//     };
+
+//     const handleOnline = () => {
+//       const lastPage = sessionStorage.getItem("lastVisitedPage");
+
+//       if (lastPage) {
+//         window.location.href = lastPage;
+//       } else {
+//         window.location.href = "/";
+//       }
+//     };
+
+//     window.addEventListener("offline", handleOffline);
+//     window.addEventListener("online", handleOnline);
+
+//     return () => {
+//       window.removeEventListener("offline", handleOffline);
+//       window.removeEventListener("online", handleOnline);
+//     };
+//   }, [router]);
+
+//   return (
+//     <>
+//       <ReactQueryProvider>
+//         <AccountLockChecker />
+
+//         {children}
+
+//         <Toaster position="top-right" richColors closeButton duration={3000} />
+//       </ReactQueryProvider>
+//     </>
+//   );
+// }
+
 "use client";
 
 import { Toaster } from "sonner";
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import { useEffect } from "react";
-import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import AccountLockChecker from "./AccountLockChecker";
 
 export default function ClientLayout({
@@ -12,22 +80,17 @@ export default function ClientLayout({
   children: React.ReactNode;
 }) {
   const router = useRouter();
-
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
-    const currentUrl =
-      pathname + (searchParams.toString() ? `?${searchParams.toString()}` : "");
-
-    // Offline page save નહીં કરવી
     if (pathname !== "/offline") {
+      const currentUrl = window.location.pathname + window.location.search;
+
       sessionStorage.setItem("lastVisitedPage", currentUrl);
     }
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   useEffect(() => {
-    // 🔥 Check initial status
     if (!navigator.onLine) {
       router.push("/offline");
     }
@@ -56,18 +119,12 @@ export default function ClientLayout({
   }, [router]);
 
   return (
-    <>
-      {/* <ReactQueryProvider>
-        {children}
-        <Toaster position="top-right" richColors closeButton duration={3000} />
-      </ReactQueryProvider> */}
-      <ReactQueryProvider>
-        <AccountLockChecker />
+    <ReactQueryProvider>
+      <AccountLockChecker />
 
-        {children}
+      {children}
 
-        <Toaster position="top-right" richColors closeButton duration={3000} />
-      </ReactQueryProvider>
-    </>
+      <Toaster position="top-right" richColors closeButton duration={3000} />
+    </ReactQueryProvider>
   );
 }
